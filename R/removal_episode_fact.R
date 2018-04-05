b73d978eca8283fb46feb22fb0df586a3dfcd5c8
NETID\jooreea <jooreea@uw.edu>
2018-04-04 17:00:15 -0700

    Reviewed and modified code

--- R/removal_episode_fact.R
+++ R/removal_episode_fact.R
@@ -1,4 +1,3 @@
-
 # COlumns needed for AFCARS EPISODE_REMOVAL_FACT
 # id_removal_epsisode_fact - this is the primary key for the table
 # id_prsn - recnumbr
@@ -13,6 +12,7 @@
 library(DBI)
 library(stringr)
 library(tidyr)
+library(magrittr)
 library(dbplyr)
 library(dplyr)
 
@@ -21,52 +21,76 @@ library(dplyr)
 con <- dbConnect(odbc::odbc(), "POC")
 
 # getting data
-
-## Joe suggested using tbl, I was having connection problems
-## and wanted to just get the script working!
-# tbl(con, "[annual_report].[ca_fc_afcars_extracts]") %>%
-#   filter(recnumbr == 4021) %>% as_data_frame()
-
-DBI::dbListTables(con)
-
-tbl(con, in_schema("annual_report", "ca_fc_afcars_extracts"))
-
-
 afcars <-
-dbGetQuery(con, "SELECT recnumbr
-	                  , repdat
-                    , rem1dt
-                    , dlstfcdt
-                    , latremdt
-                    , dodfcdt
-                    , totalrem
-                    , disreasn
-                FROM [CA_ODS].[annual_report].[ca_fc_afcars_extracts]"
-           )
+  tbl(con, in_schema("annual_report", "ca_fc_afcars_extracts")) %>%
+  dplyr::select(recnumbr
+                , repdat
+                , rem1dt
+                , dlstfcdt
+                , latremdt
+                , dodfcdt
+                , totalrem
+                , disreasn) %>%
+  collect()
 
-# function!
+# dbGetQuery(con, "SELECT recnumbr
+# 	                  , repdat
+#                     , rem1dt
+#                     , dlstfcdt
+#                     , latremdt
+#                     , dodfcdt
+#                     , totalrem
+#                     , disreasn
+#                 FROM [CA_ODS].[annual_report].[ca_fc_afcars_extracts]"
+#            )
 
-date_to_numeric <- function(x){as.numeric(str_replace_all(as.character(x), '-', ''))}
+date_to_integer <- function(x){as.integer(str_replace_all(as.character(x), '-', ''))}
 
-removal_episode_fact <- 
-  group_by(afcars, recnumbr, latremdt) %>%
-  arrange(desc(repdat)) %>%
-  mutate(eps_rnk = row_number()) %>%
+mtr <- afcars %>%
+  # get maximum number of removals for each child:
   group_by(recnumbr) %>%
-  mutate(max_totalrem = max(totalrem)) %>%
+  summarise(max_totalrem = max(totalrem, na.rm = TRUE)) %>%
+  ungroup()
+
+removal_episode_fact <- afcars %>%
+  # rank reports by last report date for each child and removal date:
+  group_by(recnumbr, latremdt) %>%
+  mutate(eps_rnk = row_number(desc(repdat))) %>%
   ungroup() %>%
-  filter(eps_rnk == 1) %>%
-  # filter(recnumbr %in% c(4021, 2740417, 1418573, 1501651)) %>%
+  # keep only last report:
+  filter(eps_rnk == 1) %>% # this makes episode-level df
   arrange(recnumbr, repdat) %>%
-  mutate(int_rem1dt = date_to_numeric(rem1dt)
-         , int_dlstfcdt = date_to_numeric(dlstfcdt)
-         , int_latremdt = date_to_numeric(latremdt)
-         , int_dodfcdt = date_to_numeric(dodfcdt)
-         , int_dodfcdt = ifelse((lead(totalrem) - totalrem) == 1 & is.na(int_dodfcdt), lead(int_dlstfcdt), int_dodfcdt)
-         , id_removal_epsisode_fact = 1:n()
-         ) %>%
-  select(id_removal_epsisode_fact 
-         , id_prsn = recnumbr
+  mutate(int_rem1dt = date_to_numeric(rem1dt) # make dates into integers
+         , int_dlstfcdt = date_to_integer(dlstfcdt)
+         , int_latremdt = date_to_integer(latremdt)
+         , int_dodfcdt = date_to_integer(dodfcdt)
+         # fill in missing discharge dates with the next episode's last discharge date:
+         , int_dodfcdt = ifelse((lead(totalrem) - totalrem) == 1 & is.na(int_dodfcdt), lead(int_dlstfcdt), int_dodfcdt))  
+
+## WILL EVENTUALLY REMOVE
+## Creating flags for problematic records
+## What should we do with records that have a totalrem starting at 0?
+problem_records <- removal_episode_fact %>%
+  group_by(recnumbr) %>%
+  mutate(latremdt_flag = ifelse(lag(int_latremdt) > int_latremdt, 1, 0)
+         , totalrem_flag = ifelse(totalrem - lag(totalrem) != 1 | min(totalrem) != 1, 1, 0)
+  )%>%
+  ungroup() %>%
+  filter(latremdt_flag == 1 | totalrem_flag == 1) 
+  
+removal_episode_fact2 <- removal_episode_fact %>%
+  ## This complete function (from tidyr) seemed like the easiest way to
+  ## create the new rows, but if anyone has a better idea, let's do it!
+  # expand df by adding row for each potential/unrecorded removal (0-18)  
+  complete(recnumbr, totalrem = seq(1, max(totalrem))) %>%
+  # join back to max_totalrem:
+  left_join(mtr) %>%
+  # keep only valid episodes:
+  filter(totalrem <= max_totalrem) %>%
+  # create unique id for each episode: 
+  mutate(id_removal_epsisode_fact = row_number()) %>%
+  # subset/rename data
+  select(id_prsn = recnumbr
          , id_cal_dim_begin = int_latremdt
          , id_cal_dim_end = int_dodfcdt
          , id_cal_dim_date_begin = latremdt
@@ -76,25 +100,6 @@ removal_episode_fact <-
          , int_dlstfcdt
          , totalrem
          , max_totalrem
-  ) %>%
-  ## WILL EVENTUALLY REMOVE
-  ## Creating flags for problematic records
-  group_by(id_prsn) %>%
-  arrange(id_removal_epsisode_fact) %>%
-  mutate(latremdt_flag = ifelse(lag(id_cal_dim_begin) > id_cal_dim_begin, 1, 0)
-         , totalrem_flag = ifelse(totalrem - lag(totalrem) != 1, 1, 0)
-         )%>%
-  ungroup()
-
-removal_episode_fact <- 
-  select(removal_episode_fact, - max_totalrem) %>% 
-  ## THis complete function (from tidyr) seemed like the easiest way to
-  ## create the new rows, but if anyone has a better idea, let's do it!
-  complete(id_prsn, totalrem = seq(min(totalrem), max(totalrem))) %>%
-  left_join(select(removal_episode_fact, id_prsn, max_totalrem) %>% distinct()) %>%
-  filter(totalrem <= max_totalrem)
-
-
-
+  )
 
 
