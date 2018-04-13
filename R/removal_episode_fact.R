cfc7a3fa963401e1a4a7270c101def688bca416c
NETID\jooreea <jooreea@uw.edu>
2018-04-13 15:17:37 -0700

    Added rows where first removal date does not have an episode record

--- R/removal_episode_fact.R
+++ R/removal_episode_fact.R
@@ -20,45 +20,66 @@ library(dplyr)
 con <- dbConnect(odbc::odbc(), "POC")
 
 # getting data
-afcars <-
-  tbl(con, in_schema("annual_report", "ca_fc_afcars_extracts")) %>%
-  dplyr::select(recnumbr
-                , repdat
-                , dob
-                , rem1dt
-                , dlstfcdt
-                , latremdt
-                , dodfcdt
-                , totalrem
-                , disreasn) %>%
-  collect()
+# afcars <-
+#   tbl(con, in_schema("annual_report", "ca_fc_afcars_extracts")) %>%
+#   dplyr::select(recnumbr
+#                 , repdat
+#                 , dob
+#                 , rem1dt
+#                 , dlstfcdt
+#                 , latremdt
+#                 , dodfcdt
+#                 , totalrem
+#                 , disreasn) %>%
+#   collect()
 
-# dbGetQuery(con, "SELECT recnumbr
-# 	                  , repdat
-#                     , rem1dt
-#                     , dlstfcdt
-#                     , latremdt
-#                     , dodfcdt
-#                     , totalrem
-#                     , disreasn
-#                 FROM [CA_ODS].[annual_report].[ca_fc_afcars_extracts]"
-#            )
+afcars <- dbGetQuery(con, "SELECT recnumbr
+	                  , repdat
+                    , dob
+                    , rem1dt
+                    , dlstfcdt
+                    , latremdt
+                    , dodfcdt
+                    , totalrem
+                    , disreasn
+                FROM [CA_ODS].[annual_report].[ca_fc_afcars_extracts]"
+           )
 
 date_to_integer <- function(x){as.integer(str_replace_all(as.character(x), '-', ''))}
 
-mtr <- afcars %>%
+max_total_removals <- afcars %>%
   # get maximum number of removals for each child:
   group_by(recnumbr) %>%
   summarise(max_totalrem = max(totalrem, na.rm = TRUE)) %>%
   ungroup()
 
+missing_first_removal <- afcars %>% 
+  # filter where first removal is missing an episode record
+  group_by(recnumbr) %>%
+  filter(all(rem1dt != latremdt)) %>%
+  ungroup() %>%
+  # keep unique first removal dates (keep last report)
+  arrange(desc(repdat)) %>%
+  distinct(recnumbr, rem1dt, .keep_all = TRUE) %>%
+  dplyr::select(recnumbr, repdat, dob, rem1dt) %>%
+  mutate(repdat = 000000, # recode repdat
+         latremdt = rem1dt) # make last removal date the first removal date
+
+
 removal_episode_fact <- afcars %>%
+  # add rows where first removal was missing an episode record
+  bind_rows(missing_first_removal) %>%
   # rank reports by last report date for each child and removal date:
   group_by(recnumbr, latremdt) %>%
   mutate(eps_rnk = row_number(desc(repdat))) %>%
   ungroup() %>%
   # keep only last report:
   filter(eps_rnk == 1) %>% # this makes episode-level df
+  group_by(recnumbr) %>%
+  # count removals on episodes that exist in the table 
+  # Use this moving forward? For now, keeping to totalrem
+  mutate(totalrem_cnt = row_number(latremdt)) %>%
+  ungroup() %>%
   arrange(recnumbr, repdat) %>%
   mutate(int_rem1dt = date_to_integer(rem1dt) # make dates into integers
          , int_dlstfcdt = date_to_integer(dlstfcdt)
@@ -79,9 +100,9 @@ removal_episode_fact <- afcars %>%
   ## This complete function (from tidyr) seemed like the easiest way to
   ## create the new rows, but if anyone has a better idea, let's do it!
   # expand df by adding row for each potential/unrecorded removal (0-18)  
-  complete(recnumbr, totalrem = seq(1, max(totalrem))) %>%
+  complete(recnumbr, totalrem = seq(1, max(totalrem, na.rm = TRUE))) %>%
   # join back to max_totalrem:
-  left_join(mtr) %>%
+  left_join(max_total_removals) %>%
   # keep only valid episodes:
   filter(totalrem <= max_totalrem) %>%
   # create unique id for each episode: 
@@ -97,8 +118,10 @@ removal_episode_fact <- afcars %>%
          , repdat
          , int_dlstfcdt
          , totalrem
+         , totalrem_cnt
          , max_totalrem
   )
 
+## still need to filter out reports outside range (2008-10-01 to 2017-09-30)
 
 
